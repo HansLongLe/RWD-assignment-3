4c9c51e24467d866776b7d71703144cbb38518fd
VostaMartinVM <74717642+VostaMartinVM@users.noreply.github.com>
2020-12-06 16:44:09 +0100

    Wateringcan rotation
    Wateringcan rotation

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream
 //MAKE THE MAGIC HAPPEN
+=======
+>>>>>>> Stashed changes
 $(document).mousemove(function (event) {
     $("#net").css({
         left: event.pageX,
@@ -6,7 +9,26 @@ $(document).mousemove(function (event) {
     });
 });
 
+<<<<<<< Updated upstream
 $("#apple1").on("click", function () {
 
+=======
+$(document).ready(function()
+{
+   var rotated = false;
+   $('#wateringcan').on('click',function()
+   {
+        rotated = !rotated;
+        if(rotated)
+        {
+            $(this).addClass("Rotate")
+        }
+        else
+        {
+            $(this).addClass("Rotateback")
+            rotated = false;
+        }
+   }); 
+>>>>>>> Stashed changes
 
 });
