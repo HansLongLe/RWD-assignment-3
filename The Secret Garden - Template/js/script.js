7a3c6baf1cbdac2827d4d27ea7cc5359324f3039
HansLongLe <HansLongLe@gmail.com>
2020-12-06 17:58:44 +0100

    Butterfly animation

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-//MAKE THE MAGIC HAPPEN
-=======
->>>>>>> Stashed changes
 $(document).mousemove(function (event) {
     $("#net").css({
         left: event.pageX,
@@ -9,26 +5,72 @@ $(document).mousemove(function (event) {
     });
 });
 
-<<<<<<< Updated upstream
-$("#apple1").on("click", function () {
 
-=======
-$(document).ready(function()
-{
-   var rotated = false;
-   $('#wateringcan').on('click',function()
-   {
+$(document).ready(function () {
+    var rotated = false;
+    $('#wateringcan').on('click', function () {
         rotated = !rotated;
-        if(rotated)
-        {
+        if (rotated) {
             $(this).addClass("Rotate")
-        }
-        else
-        {
+        } else {
             $(this).addClass("Rotateback")
             rotated = false;
         }
-   }); 
->>>>>>> Stashed changes
+    });
+});
+
+//butterfly
+$(document).ready(function () {
+    animateButterfly();
 
 });
+
+function makeNewPosition() {
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 200;
+    var w = $(window).width() - 200;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+}
+
+function animateButterfly() {
+    var newq = makeNewPosition();
+    $('#butterfly').animate({
+        top: newq[0],
+        left: newq[1]
+    }, 3000, function () {
+        animateButterfly();
+    });
+
+};
+$('#butterfly').hover(function () {
+    butterflyOnHover();
+})
+
+function butterflyOnHover() {
+    var newq = makeNewPosition();
+    $('#butterfly').animate({
+        top: newq[0],
+        left: newq[1]
+    }, 500, function () {
+        animateButterfly();
+    });
+};
+$('#butterfly').mouseover(function () {
+    butterflyMouseOver();
+})
+
+function butterflyMouseOver() {
+    var newq = makeNewPosition();
+    $('#butterfly').animate({
+        top: newq[0],
+        left: newq[1]
+    }, 3000, function () {
+        animateButterfly();
+    });
+};
